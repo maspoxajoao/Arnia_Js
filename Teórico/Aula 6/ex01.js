350f2adbdffd5bb84a8e7bdbad3f590680e615b0
João Victor <116379577+maspoxajoao@users.noreply.github.com>
2023-02-28 19:15:01 -0300

    11

---
+++ Teórico/Aula 6/ex01.js
@@ -0,0 +1,7 @@
+let io = require("../../io");
+
+let num = 0;
+while (num < 3) {
+  console.log("Brasil");
+  num = num + 1;
+}
---
+++ Teórico/Aula 6/ex02.js
@@ -0,0 +1,9 @@
+let io = require("../../io");
+
+let N = 0
+let i = 0
+while (i < 3 ) {
+    i = i + 1
+    console.log(i)
+    
+}
