c378dbec1fd9f73bf59118ed4f57ef533643d4b6
João Victor <116379577+maspoxajoao@users.noreply.github.com>
2023-03-01 18:22:02 -0300

    aa

---
+++ Prático/Aula 5/ex1.js
@@ -0,0 +1,16 @@
+let io = require("../../io");
+
+console.log("Digite o numeor de eleitores:");
+let totalEleitores = io.read();
+console.log("Digite o numero de votos em branco");
+let votosBrancos = io.read();
+console.log("Digite o numero votos nulos");
+let votosNulos = io.read();
+console.log("Digite o numero de votos validos:");
+let votosValidos = io.read():
+
+let perBranco = (votosBrancos / totalEleitores) * 100:
+let perNulos = (votosNulos / totalEleitores) * 100;
+let perValidos = (votosValidos / totalEleitores ) * 100:
+
+console.log(` o percentual de votos é ${perBranco}% em branco, ${perNulos}% em nulos, ${perValidos}% em validos`)
