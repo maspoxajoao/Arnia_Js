8213812d2ad7c80d56740e2226384d7b1aa71eca
João Victor <116379577+maspoxajoao@users.noreply.github.com>
2023-03-22 21:23:43 -0300

    s

--- Prático/Aula 18/app.js
+++ Prático/Aula 18/app.js
@@ -1,14 +1,3 @@
-const timestamp = (new Date()).getTime()
-const publicKey = 'd0d76a60dcc1d76a2227bc2debaca41c'
-const privateKey = '92eab95f3d5ee3fc201dbf74e7d984192599f316'
-const hash = timestamp + privateKey + publicKey
-const hashMd5 = MD5.generate(hash)
+const apiKey = '5ILEQLOApbeoYD3KR22ZJIA8SK50XjIH';
+const baseURL = 'http://dataservice.accuweather.com/'
 
-
-buscarPersonagens = async () =>{
-  const resposta = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${publicKey}&ts=${timestamp}&hash=${hashMd5}`)
-  const personagens = await resposta.json()
-  console.log(personagens)
-}
-
-buscarPersonagens()
