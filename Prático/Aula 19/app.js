85fe4513e28e977b567255e570a6d645d6597c3e
João Victor <116379577+maspoxajoao@users.noreply.github.com>
2023-03-28 18:20:51 -0300

    aa

--- Prático/Aula 19/app.js
+++ Prático/Aula 19/app.js
@@ -1,45 +1,60 @@
+const openModalBtn = document.getElementById("novoPost");
+const closeBtn = document.getElementById("xd");
+const modal = document.getElementById("myModal");
+let posts = []
+
+const abrir = () => {
+  modal.style.display = "block"
+}
+
+const fechar = () => {
+  modal.style.display = "none"
+}
+
+closeBtn.addEventListener("click", fechar)
+
+
 const getPosts = async () => {
-  const apiResponse = await fetch("http://localhost:3000/posts");
-  const posts = await apiResponse.json();
-  console.log(posts);
-};
-
-const createPost = async (post) => {
-  await fetch("http://localhost:3000/posts", {
-    method: "POST",
-    headers: {
-      "Accept": "application/json, text/plain, */*",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(post),
-  });
-};
-
-const addPost = async () => {
-  const post = {
-    titulo: document.getElementById("txTitulo").value,
-    autor: document.getElementById("txtAutor").value,
-    imagem: document.getElementById("txtimg").value,
-    texto: document.getElementById("txtArea").value,
+    const apiResponse = await fetch("http://localhost:3000/posts");
+    const posts = await apiResponse.json();
+    console.log(posts);
   };
-
-  await createPost(post);
-  const titulo = post.titulo;
-  const autor = post.autor;
-  const imagem = post.imagem;
-  const texto = post.texto;
-
-  const div = document.getElementById("noticia");
-  div.innerHTML += `
-    <div>
-   <img src="${imagem}" alt="imagem">
-    <div>
-    <h1>${titulo}</h1>
-    <p> ${texto}</p>
-    </div>
-    </div>
-  `;
-};
-
-
-
+  
+  const createPost = async (post) => {
+    await fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post),
+    });
+  };
+  
+  const addPost = async () => {
+    const post = {
+      titulo: document.getElementById("txTitulo").value,
+      autor: document.getElementById("txtAutor").value,
+      imagem: document.getElementById("txtimg").value,
+      texto: document.getElementById("txtArea").value,
+    };
+  
+    await createPost(post);
+    const titulo = post.titulo;
+    const autor = post.autor;
+    const imagem = post.imagem;
+    const texto = post.texto;
+  
+    const div = document.getElementById("noticia");
+    div.innerHTML += `
+      <div>
+     <img src="${imagem}" alt="imagem">
+      <div>
+      <h1>${titulo}</h1>
+      <p> ${texto}</p>
+      </div>
+      </div>
+    `;
+  };
+  
+ 
